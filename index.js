3c602832168ebfaad79b78e408f3209b055ae10d
Xotic750 <xotic750@gmail.com>
2017-09-09 01:50:37 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @file An ES6 Object.keys shim.
- * @version 2.1.0
+ * @version 2.2.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
