30b8829d507761f4682479edb23d668a2d2e5b41
Graham Fairweather <xotic750@gmail.com>
2019-08-06 23:20:38 +0200

    Add coveralls badge

--- dist/object-keys-x.js
+++ dist/object-keys-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-06T00:53:29.533Z",
+  "date": "2019-08-06T21:20:32.532Z",
   "describe": "",
   "description": "An ES6 Object.keys shim.",
   "file": "object-keys-x.js",
