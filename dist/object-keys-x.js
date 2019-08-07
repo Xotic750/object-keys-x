d96676e152984d0c33784193dc1939d7acd2f84b
Graham Fairweather <xotic750@gmail.com>
2019-08-07 03:33:33 +0200

    Auto build

--- dist/object-keys-x.js
+++ dist/object-keys-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-06T21:20:32.532Z",
+  "date": "2019-08-07T01:33:26.982Z",
   "describe": "",
   "description": "An ES6 Object.keys shim.",
   "file": "object-keys-x.js",
